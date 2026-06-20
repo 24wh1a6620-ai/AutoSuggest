087fc02df0bd211837a84484bca3b45bb7b7baa8
glorypranavi <24wh1a6620@bvrithyderabad.edu.in>
2026-06-20 10:01:26 +0530

    created toggle card

---
+++ frontend/js/toggle.js
@@ -0,0 +1,21 @@
+var users=[
+    {
+        "name":"john doe",
+        "gender": "male",
+        "image":"/image/john.png"
+    },
+    {
+        "name":"jane doe",
+        "gender": "female",
+        "image":"/image/jane.png"
+    }
+]
+var curId=0;
+
+function toggle(){
+   curId=(curId+1)%2;
+  var user=users[ curId ];
+   document.getElementById("user-img").src=user.image;
+   document.getElementById("user-name").innerText=user.name;
+   document.getElementById("user-gender").innerText=user.gender;
+}
